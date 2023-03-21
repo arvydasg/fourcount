f57412e319f6f4288869e13da00e3910df539666
arvydasg <azegaspa@gmail.com>
2023-03-21 22:01:45 +0200

    starter code for DB

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+print(basedir)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "db.db")
+db = SQLAlchemy(app)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
