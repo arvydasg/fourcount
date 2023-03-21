035ab9dee34f04868c2c035a863b90f227129132
arvydasg <azegaspa@gmail.com>
2023-03-21 22:47:14 +0200

    created user model, created populate.py to fill date into user table

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
 
 with app.app_context():
