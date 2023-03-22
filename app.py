ba30cdb9616236e0c7200fd6bb2afab2bb144f86
arvydasg <azegaspa@gmail.com>
2023-03-22 21:56:13 +0200

    add password validation

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, FloatField, DecimalField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, EqualTo
 import os
 from datetime import datetime
 from flask_bcrypt import Bcrypt
@@ -158,7 +158,7 @@ def route_bills(group_id):
         db.session.add(new_bill)
         db.session.commit()
         flash("New bill added successfully.")
-        return redirect(url_for("route_bills", group_id=group_id))
+        return redirect(url_for("add_bill", group_id=group_id))
 
     return render_template("bills.html", group=group, bills=bills, form=form)
 
@@ -189,6 +189,13 @@ class RegisterForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password",
+        validators=[
+            DataRequired(),
+            EqualTo("password", message="Passwords must match."),
+        ],
+    )
     submit = SubmitField("Submit")
 
     def validate_name(self, name):
